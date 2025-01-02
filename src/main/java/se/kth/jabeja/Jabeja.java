017f4f018749fdc62803cf1ada1abe8ea4e0b8ff
harrietmanninger <89923321+harrietmanninger@users.noreply.github.com>
2025-01-02 21:24:26 +0100

    Update Jabeja.java

--- src/main/java/se/kth/jabeja/Jabeja.java
+++ src/main/java/se/kth/jabeja/Jabeja.java
@@ -56,7 +56,7 @@ public class Jabeja {
     float T_min = 0.00001f;
     //float T_min = 0.001f;
     //float aplha2 = 0.9f;
-    float aplha2 = 0.8f;
+    float aplha2 = 0.99f;
     if(T > T_min)
       T *= aplha2;
     
